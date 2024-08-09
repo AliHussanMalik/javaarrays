567523b70e852265b64f0597385b9506b88cbfd6
Ali Hussan Malik <malikalihussan2@gmail.com>
2024-08-09 19:37:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const students = [
+  { name: "Alice", scores: [90, 85, 92] },
+  { name: "Bob", scores: [75, 80, 85] },
+  { name: "Charlie", scores: [90, 95, 85] },
+  { name: "Jack", scores: [100, 100, 100] },
+];
+students.map(students)
+ let average=students.scores.map(function(currentvalue,index,arr)thisvalue)=>()
